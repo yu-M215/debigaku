c68b52baaf1c95ac756911d6f43166637b6a95da
yu-M215 <yuu0609km.215@gmail.com>
2022-07-28 23:10:50 +0900

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
